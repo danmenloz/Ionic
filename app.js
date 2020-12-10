3fd7c80d6f2f4da6beb30cc2637af2a386088a68
danmenloz <mendez.201203657@gmail.com>
2020-12-09 23:33:10 -0500

    BMI calculator.

---
+++ app.js
@@ -0,0 +1,15 @@
+const calculateBtn = document.querySelector('ion-button');
+const heightInput = document.getElementById('height-input');
+const weightInput = document.getElementById('weight-input');
+
+const calculateBmi = () => {
+    const enteredHeight = +heightInput.value;// + converts string to number
+    const enteredWeight = +weightInput.value;
+
+    const bmi = enteredWeight / (enteredHeight * enteredHeight); 
+
+    console.log(bmi);
+};
+
+calculateBtn.addEventListener('click',calculateBmi);
+
